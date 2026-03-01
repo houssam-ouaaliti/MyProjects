801be8296bc62a5220ecfdac69067ed77f6683a0
Houssam Ouaaliti <houssamouaaliti222005@gmail.com>
2026-03-01 15:38:56 +0100

    laravel project

---
+++ laravel-test/app/Models/Booking.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Booking extends Model
+{
+    use HasFactory;
+
+    // TRÈS IMPORTANT : Ajoute cette ligne pour autoriser l'enregistrement
+    protected $fillable = [
+        'user_id', 
+        'property_id', 
+        'start_date', 
+        'end_date'
+    ];
+
+    public function property() {
+        return $this->belongsTo(Property::class);
+    }
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+}
---
+++ minishell/tube/tube4/tube4.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <signal.h>
+
+int N = 10;
+int val;
+int lus;
+
+int main(void) {
+    int tube[2];
+    pipe(tube);
+
+    pid_t pidFils = fork();
+
+    switch (pidFils) {
+        case -1:
+            perror("fork");
+            exit(EXIT_FAILURE);
+
+        case 0:
+            close(tube[1]);
+            while ((lus = read(tube[0], &val, sizeof(val))) > 0) {
+                printf("valeur = %d\n", val);
+                printf("valeur_read = %d\n", lus);
+            }
+            printf("sortie de la boucle\n");
+            close(tube[0]);
+            exit(EXIT_SUCCESS);
+
+        default:
+            close(tube[0]);
+            for (val = 0; val < N; val++) {
+                write(tube[1], &val, sizeof(val));
+            }
+            close(tube[1]);
+            sleep(10);
+    }
+
+    return EXIT_SUCCESS;
+}
+
